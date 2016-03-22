68402cbd7f42294cef336192b4498d08ceb4cb9f
Kevin Finelli <kevin.finelli@cern.ch>
2016-03-22 00:37:44 +0000

    tmux 1.x script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
+if tmux -V | grep "1." 2>&1; then
+  ln -s $HOME/.dotfiles/tmux.legacy.conf $HOME/.tmux.conf 
+elif command -v tmux  2>&1; then 
+  ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
+fi
+
+#&& tmux -V | grep "1."> /dev/null  2>&1 || 
