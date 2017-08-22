a437d891f1f5e550f7806f2a1ed7a012ab711144
Kevin Finelli <kevin.finelli@cern.ch>
2017-08-22 09:30:19 -0400

    Add zlogout

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/zlogout $HOME/.zlogout
 if tmux -V | grep "1." 2>&1; then
   ln -s $HOME/.dotfiles/tmux.legacy.conf $HOME/.tmux.conf 
 elif command -v tmux  2>&1; then 
